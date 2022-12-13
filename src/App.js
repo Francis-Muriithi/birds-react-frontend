7f980a408807d473c3fe2dc1d7ba725c41b6b771
Francis-Muriithi <mufraggie@gmail.com>
2022-12-13 11:44:11 +0300

    set-up

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Create from './Create';
+
+function App() {
+  return (
+    <div className='main'>
+      <h2 className='main-header'>React My Birds App</h2>
+      <div>
+        <Create/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
